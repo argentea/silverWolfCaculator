1c1083c751717de4b9e0104d230395f312d2e8c9
kunpeng <kunpeng.jiang@intel.com>
2023-06-23 07:22:49 +0800

    Add basic database

---
+++ src/database/database.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <string>
+
+namespace database {
+
+typedef double statv_t;
+typedef int level_t;
+
+// character_level_coefficient
+// character_ascend_coefficient
+const statv_t ch_lv_co = 0.05;
+const statv_t ch_ac_co = 0.4;
+// There're 6 gaps;
+const statv_t ch_lv_max = 80;
+const statv_t ch_ac_max = 6;
+
+// light_cone_level_coefficient
+// light_cone_ascend_coefficient
+const statv_t lc_lv_co = 0.05;
+const statv_t lc_ac_co = 0.4;
+// There're 6 gaps;
+const statv_t lc_lv_max = 80;
+const statv_t lc_ac_max = 6;
+
+
+class LightCone {
+  private:
+    std::string _name;
+    std::string _cn_name;
+    statv_t _base_ATK;
+    statv_t _base_DEF;
+    statv_t _base_HP;
+    level_t _level;
+    level_t _ascend;
+  public:
+    // Init without level
+    LightCone(std::string& name, std::string& cn_name, statv_t base_ATK,
+              statv_t base_DEF, statv_t base_HP);
+
+    // We set ascend as biggest as possible for current level.
+    void set_level(level_t level);
+    // This function should be used only on gap level.
+    void set_ascend(level_t ascend);
+
+};
+
+class Character {
+  private:
+    std::string _name;
+    std::string _cn_name;
+    statv_t _base_ATK;
+    statv_t _base_DEF;
+    statv_t _base_HP;
+    statv_t _base_SPD;
+    level_t _level;
+    level_t _ascend;
+    LightCone& light_cone;
+  public:
+    Character(std::string& name, std::string& cn_name, statv_t base_ATK,
+              statv_t base_DEF, statv_t base_HP, statv_t base_SPD);
+
+    // We set ascend as biggest as possible for current level.
+    void set_level(level_t level);
+    // This function should be used only on gap level.
+    void set_ascend(level_t ascend);
+};
+
+
+};
---
+++ src/database/parser.h
@@ -0,0 +1,4 @@
+
+
+
+int read_charactor(const char* file_name);
