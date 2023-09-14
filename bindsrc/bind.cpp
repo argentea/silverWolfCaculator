b507e54c33d6a3ee20665f8124018bd0b2fe9b1c
kunpeng <kunpeng.jiang@intel.com>
2023-09-14 14:39:59 +0000

    Remove cadd

--- bindsrc/bind.cpp
+++ bindsrc/bind.cpp
@@ -1,10 +1,4 @@
-#include"cAdd.h"
 #include"pybind11/pybind11.h"
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(pybindCmake, m) {
-	py::class_<Cadd>(m, "Cadd")
-		.def(py::init<int, int>())
-		.def("add", &Cadd::add);
-}
--- tools/test.py
+++
@@ -1,4 +0,0 @@
-from pyCode import pyAdd
-
-a = pyAdd(10, 20)
-print(a.add())
